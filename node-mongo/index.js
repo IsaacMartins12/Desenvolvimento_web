a764a1a339ca90850589e36765f4ad1f5a8b84cb
Isaac Martins <91567120+IsaacMartins12@users.noreply.github.com>
2023-06-12 22:05:02 -0400

    Node js com mongo DB

---
+++ node-mongo/index.js
@@ -0,0 +1,48 @@
+// configuracao inicial 
+require('dotenv').config()
+const express = require('express')
+const { default: mongoose } = require('mongoose')
+const app = express()
+
+
+// forma de ler JSON / middlewares
+
+app.use(
+    express.urlencoded({
+        extended : true,
+    }),
+)
+
+app.use(express.json())
+
+// Rotas da API
+
+const personRoutes = require('./routes/personRoute')
+
+app.use('/person',personRoutes)
+
+// rota inicial / endpoint
+
+app.get("/" , (req, res)=>{
+
+    // Mostrar req
+    
+    res.json({message : "OI Express !"})
+    
+
+})
+
+// entregar uma porta 
+
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+
+mongoose.connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.fx6ekbw.mongodb.net/?retryWrites=true&w=majority`
+).then(()=>{
+    console.log("Conectado ao banco de dados !")
+    app.listen(3000)
+})
+.catch((err)=>{
+  console.log(err)
+})
